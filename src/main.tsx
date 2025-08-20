83d7f8c318dd948464e3cff25bd21cea14c9194c
yabluninn <yablunin.artem@gmail.com>
2025-08-20 20:39:42 +0300

    final issue fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-// main.tsx или App.tsx
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-<React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
-</React.StrictMode>;
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
